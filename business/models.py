7e7688e3e86218ee91ca52edd8a9070bcf9eb1ee
pydevazmi <pydevazmi@gmail.com>
2024-03-02 15:02:56 +0200

    Adding New App To Manage Accounts

--- business/models.py
+++ business/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext as _
 from django.utils.text import slugify
 
 # Create your models here.
-YARN_UNIT_TYPE = (
+UNIT_TYPE = (
     (_("Cartoon"), _("Cartoon")),
     (_("Bag"), _("Bag")),
     (_("Box"), _("Box")),
@@ -31,7 +31,7 @@ class YarnInventory(models.Model):
     owner = ''
     supplier = ''
     total_weight_in_kg = ''
-    actual_weight = ''
+    actual_existing_weight = ''
     unit_type = ''
     quantity_per_unit = ''
     price_per_kg = ''
@@ -42,8 +42,8 @@ class YarnInventory(models.Model):
     slug = ''
 
     class Meta:
-        verbose_name = _("YarnInventory")
-        verbose_name_plural = _("YarnsInventory")
+        verbose_name = _("Yarn In Inventory")
+        verbose_name_plural = _("Yarns In Inventory")
 
     def __str__(self):
         return f"{self.yarn} {self.supplier}"
@@ -62,10 +62,11 @@ class YarnFactory(models.Model):
     orderd_at = ''
     manufactured_fabric = ''
     fabric_weight = ''
+    status = ''
 
     class Meta:
-        verbose_name = _("YarnFactory")
-        verbose_name_plural = _("YarnsFactory")
+        verbose_name = _("Yarn In Factory")
+        verbose_name_plural = _("Yarns In Factories")
 
     def __str__(self):
         return self.yarn
@@ -94,16 +95,17 @@ class FabricInventory(models.Model):
     fabric = ''
     factory = ''
     total_weight =''
-    quantity_recieved = ''
-    quantity_remaining = ''
+    manufactured_quantity_recieved = ''
+    manufactured_quantity_remaining = ''
     pieces = ''
     recieved_at = ''
     notes = ''
+    status = ''
     slug = ''
 
     class Meta:
-        verbose_name = _("FabricInventory")
-        verbose_name_plural = _("FabricsInventory")
+        verbose_name = _("Fabric In Inventory")
+        verbose_name_plural = _("Fabrics In Inventory")
 
     def __str__(self):
         return self.fabric
@@ -111,19 +113,68 @@ class FabricInventory(models.Model):
     def get_absolute_url(self):
         return reverse("FabricInventory_detail", kwargs={"pk": self.pk})
 
+class FabricDyeingInventory(models.Model):
+    owner = ''
+    fabric = ''
+    dyeing_factory = ''
+    total_weight =''
+    dyed_quantity_recieved = ''
+    dyed_quantity_remaining = ''
+    pieces = ''
+    recieved_at = ''
+    notes = ''
+    status = ''
+    slug = ''
 
-class FabricDyeing(models.Model):
+    class Meta:
+        verbose_name = _("Dyed fabric in stock")
+        verbose_name_plural = _("Dyed fabrics in stock")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("FabricDyeingInventory_detail", kwargs={"pk": self.pk})
+
+
+class FabricDyeingFactory(models.Model):
     fabric = ''
     dyeing_factory = ''
     weigth = ''
-    came_out = ''
+    unit_type = ''
+    quantity_per_unit = ''
+    came_out_at = ''
 
     class Meta:
-        verbose_name = _("FabricDyeing")
-        verbose_name_plural = _("FabricsDyeing")
+        verbose_name = _("Fabric that went for Dyeing Factory")
+        verbose_name_plural = _("Fabrics that went for Dyeing Factories")
 
     def __str__(self):
         return self.fabric
 
     def get_absolute_url(self):
-        return reverse("FabricDyeing_detail", kwargs={"pk": self.pk})
+        return reverse("FabricDyeingFactory_detail", kwargs={"pk": self.pk})
+
+
+class SoldFabric(models.Model):
+    fabric = ''
+    customer = ''
+    weigth = ''
+    unit_type = ''
+    quantity_per_unit = ''
+    price_per_kg = ''
+    total_price = ''
+    sold_at = ''
+    notes = ''
+    slug = ''
+    
+
+    class Meta:
+        verbose_name = _("Sold Fabric")
+        verbose_name_plural = _("Sold Fabrics")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("SoldFabric_detail", kwargs={"pk": self.pk})
